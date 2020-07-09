ce63d04fb4020ead55846beeebb422d0d5e7074c
Thameemul Anzari <thameemul.anzari@ministrybrands.com>
2020-07-09 23:12:38 +0530

    ng deploy fix2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
     production: true,
+    firebaseConfig: {
+        apiKey: 'AIzaSyAGg-38iyUzqFNervz2DmKO283lTysAvwU',
+        authDomain: 'smeadviser-2320c.firebaseapp.com',
+        databaseURL: 'https://smeadviser-2320c.firebaseio.com',
+        projectId: 'smeadviser-2320c',
+        storageBucket: 'smeadviser-2320c.appspot.com',
+        messagingSenderId: '976252703911',
+        appId: '1:976252703911:web:ac923baf8604af38687782',
+        measurementId: 'G-ZD6V96R7LV',
+    },
 };
